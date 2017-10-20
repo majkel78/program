44fcfe5ac266f673a19c1027d01c854d13eb8efe
majkel78 <michaljarz78@gmail.com>
2017-10-20 02:54:28 -0700

    pobieranie znakow z konsoli, wykonywanie warunkowe

--- program3.c
+++ program3.c
@@ -13,15 +13,19 @@ int main(void)
 
 	char wybor;
 
-	printf("Podaj * jesli chcesz mnozyc: ");
+	printf("Podaj *,+ jesli chcesz mnozyc lub dodawac liczby %d i %d: ",a,b);
 	scanf("%c",&wybor);
 	if(wybor == '*')
 	{
 		printf("Wynik mnozenia: %d\n",mnoz(a,b));
 	}
-	else
+	else if(wybor == '+')
 	{
 		printf("Wynik dodawania: %d\n",dodaj(a,b));
+	}
+	else
+	{
+		printf("Podales nieodpowiedni znak!\n");
 	}
 
 	return 0;
